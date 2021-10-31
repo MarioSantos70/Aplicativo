8f5e24db18df2140d570ab33049cfb788764ae06
MarioSantos70 <92730485+MarioSantos70@users.noreply.github.com>
2021-10-31 15:14:40 -0300

    A

---
+++ www/js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyBMOhlof0Jo2P9yKKVC2RTkbCN-WVVfKOA",
+  authDomain: "app1-6dff6.firebaseapp.com",
+  projectId: "app1-6dff6",
+  storageBucket: "app1-6dff6.appspot.com",
+  messagingSenderId: "488738599977",
+  appId: "1:488738599977:web:028d856a1a81cec2d89838"
+ };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
